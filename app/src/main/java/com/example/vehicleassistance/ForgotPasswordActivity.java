a8002e8b97405e79e0ec433d178475adc996e5df
prashant bhandari <prashantbhandari1999@gmail.com>
2019-08-12 01:14:41 +0530

    Forgot Password added

---
+++ app/src/main/java/com/example/vehicleassistance/ForgotPasswordActivity.java
@@ -0,0 +1,73 @@
+package com.example.vehicleassistance;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class ForgotPasswordActivity extends AppCompatActivity {
+
+    private EditText emailEditText;
+    private Button resetPasswordButton;
+    private FirebaseAuth firebaseAuth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_forgot_password);
+        emailEditText = (EditText) findViewById(R.id.email_edittext_forgot_password_activity);
+        resetPasswordButton = (Button) findViewById(R.id.resetpassword_button);
+        firebaseAuth = FirebaseAuth.getInstance();
+        resetPassword();
+
+    }
+
+    private void resetPassword() {
+        resetPasswordButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String email = emailEditText.getText().toString();
+                if (!email.isEmpty()) {
+                    firebaseAuth.sendPasswordResetEmail(email).addOnCompleteListener(new OnCompleteListener<Void>() {
+                        @Override
+                        public void onComplete(@NonNull Task<Void> task) {
+                            if (task.isSuccessful()) {
+                                Toast.makeText(ForgotPasswordActivity.this, "Reset password email sent ", Toast.LENGTH_SHORT).show();
+                                Intent launchNextActivity;
+                                launchNextActivity = new Intent(ForgotPasswordActivity.this, MainActivity.class);
+                                launchNextActivity.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                                launchNextActivity.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                                launchNextActivity.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
+                                startActivity(launchNextActivity);
+                                finish();
+                            } else {
+
+                                Toast.makeText(ForgotPasswordActivity.this, task.getException().toString(), Toast.LENGTH_SHORT).show();
+                            }
+                        }
+
+
+                    });
+
+                } else {
+                    Toast.makeText(ForgotPasswordActivity.this, "Enter the email", Toast.LENGTH_SHORT).show();
+                }
+            }
+
+
+        });
+
+
+    }
+
+
+}
