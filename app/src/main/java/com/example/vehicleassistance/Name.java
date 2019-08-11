4f4007c14d58c70a96723cb1a270c6ce64e16e9b
prashant bhandari <prashantbhandari1999@gmail.com>
2019-08-11 20:43:52 +0530

    Name activity Updated

--- app/src/main/java/com/example/vehicleassistance/Name.java
+++ app/src/main/java/com/example/vehicleassistance/Name.java
@@ -14,26 +14,32 @@ import com.google.android.material.textfield.TextInputEditText;
 public class Name extends AppCompatActivity {
     String FirstName;
     String LastName;
-    TextInputEditText firstName,lastName;
+    TextInputEditText firstName, lastName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_name);
-        firstName=(TextInputEditText)findViewById(R.id.editText_first_name);
-        lastName=(TextInputEditText)findViewById(R.id.editText_last_name);
+        firstName = (TextInputEditText) findViewById(R.id.editText_first_name);
+        lastName = (TextInputEditText) findViewById(R.id.editText_last_name);
 
-        Button nextButton=(Button)findViewById(R.id.button_next_Name_Activity);
+        Button nextButton = (Button) findViewById(R.id.button_next_Name_Activity);
         nextButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                FirstName=firstName.getText().toString().trim();
-                LastName=lastName.getText().toString().trim();
-                if(TextUtils.isEmpty(FirstName)|| TextUtils.isEmpty(LastName)){
-                    Toast.makeText(Name.this,"Please enter valid credentials",Toast.LENGTH_SHORT).show();
+                FirstName = firstName.getText().toString().trim();
+                LastName = lastName.getText().toString().trim();
+                if (TextUtils.isEmpty(FirstName)) {
+                    firstName.setError("Please Enter FirstName");
                 }
-                else {
+                else if (TextUtils.isEmpty(LastName)) {
+                    lastName.setError("Please Enter LastName");
+                }
+                else{
                     Intent intent = new Intent(Name.this, Email.class);
+                    intent.putExtra("firstName",FirstName);
+                    intent.putExtra("lastName",LastName);
+
                     startActivity(intent);
                 }
             }
