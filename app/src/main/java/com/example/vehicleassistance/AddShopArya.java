49a23bbea8e31173e1cf1ad6aa9721d3e18b1be1
prashant bhandari <prashantbhandari1999@gmail.com>
2019-09-14 17:50:56 +0530

    AddShop Added

--- app/src/main/java/com/example/vehicleassistance/AddShopArya.java
+++ app/src/main/java/com/example/vehicleassistance/AddShopArya.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class AddShop extends AppCompatActivity {
+public class AddShopArya extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_add_shop);
+        setContentView(R.layout.activity_add_shop_arya);
     }
 }
