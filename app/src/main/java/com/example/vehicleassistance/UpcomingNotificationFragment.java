b9d7c438153116468d15debea10a3737ba4032e9
prashant bhandari <prashantbhandari1999@gmail.com>
2019-10-04 16:14:17 +0530

    Bottom naviagation error solved

--- app/src/main/java/com/example/vehicleassistance/UpcomingNotificationFragment.java
+++ app/src/main/java/com/example/vehicleassistance/UpcomingNotificationFragment.java
@@ -1,9 +1,12 @@
 package com.example.vehicleassistance;
 
+import android.content.Context;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
@@ -17,24 +20,63 @@ public class UpcomingNotificationFragment extends Fragment {
     View view;
     private RecyclerView recyclerView;
     private List<upcoming_notifications> list;
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
+    private UpcomingNotificationFragment.OnFragmentInteractionListener mListener;
+
     public UpcomingNotificationFragment() {
     }
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        list=new ArrayList<>();
-        list.add(new upcoming_notifications("!st Notification","Hello this is your notif","30/04/1999",R.drawable.service_center));
+        list = new ArrayList<>();
+        list.add(new upcoming_notifications("!st Notification", "Hello this is your notif", "30/04/1999", R.drawable.service_center));
     }
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        view=inflater.inflate(R.layout.fragment_upcoming_notification,container,false);
+        view = inflater.inflate(R.layout.fragment_upcoming_notification, container, false);
         recyclerView = view.findViewById(R.id.RecylcerView_upcoming_notification);
-         RecyclerViewAdapterForUpcomingNotifications adapter=new RecyclerViewAdapterForUpcomingNotifications(getContext(),list);
+        RecyclerViewAdapterForUpcomingNotifications adapter = new RecyclerViewAdapterForUpcomingNotifications(getContext(), list);
         recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
         recyclerView.setAdapter(adapter);
         return view;
     }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        if (context instanceof UpcomingNotificationFragment.OnFragmentInteractionListener) {
+            mListener = (UpcomingNotificationFragment.OnFragmentInteractionListener) context;
+        } else {
+            throw new RuntimeException(context.toString()
+                    + " must implement OnFragmentInteractionListener");
+        }
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        mListener = null;
+    }
+
+    /**
+     * This interface must be implemented by activities that contain this
+     * fragment to allow an interaction in this fragment to be communicated
+     * to the activity and potentially other fragments contained in that
+     * activity.
+     * <p>
+     * See the Android Training lesson <a href=
+     * "http://developer.android.com/training/basics/fragments/communicating.html"
+     * >Communicating with Other Fragments</a> for more information.
+     */
+    public interface OnFragmentInteractionListener {
+        // TODO: Update argument type and name
+        void onFragmentInteraction(Uri uri);
+    }
 }
+
