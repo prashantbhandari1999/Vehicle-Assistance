f7978ef909d69404336d646a4aebf66e1737f88a
aryaskulkarni07 <aryaskulkarni07@gmail.com>
2019-10-15 22:07:26 +0530

    Icons for Spare Parts Added Links send via Email

--- app/src/main/java/com/example/vehicleassistance/ExampleSpareParts.java
+++ app/src/main/java/com/example/vehicleassistance/ExampleSpareParts.java
@@ -1,29 +1,24 @@
 package com.example.vehicleassistance;
 
+
 public class ExampleSpareParts {
     private int mImageResource;
     private String mText1;
-    private String mText2;
 
 
-    public ExampleSpareParts(int imageResource, String text1,String text2) {
+
+    public ExampleSpareParts(int imageResource, String text1) {
         mImageResource = imageResource;
         mText1 = text1;
-        mText2 = text2;
-
-
     }
 
     public int getImageResource() {
         return mImageResource;
     }
 
+
     public String getText1() {
         return mText1;
-    }
-
-    public String getText2() {
-        return mText2;
     }
 
 }
--- app/src/main/java/com/example/vehicleassistance/Spare_Parts_Adapter.java
+++ app/src/main/java/com/example/vehicleassistance/Spare_Parts_Adapter.java
@@ -5,8 +5,6 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
-
-import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.ArrayList;
@@ -17,13 +15,15 @@ public class Spare_Parts_Adapter extends RecyclerView.Adapter<Spare_Parts_Adapte
     public static class ExampleViewHolder extends RecyclerView.ViewHolder {
         public ImageView mImageView;
         public TextView mTextView1;
-        public TextView mTextView2;
+//        public TextView mTextView2;
+
 
         public ExampleViewHolder(View itemView) {
             super(itemView);
             mImageView = itemView.findViewById(R.id.imageView_sparepart);
-            mTextView1 = itemView.findViewById(R.id.textView_sparepart);
-            mTextView2 = itemView.findViewById(R.id.textView_sparepart_price);
+            mTextView1 = itemView.findViewById(R.id.textView_spare);
+//          mTextView2 = itemView.findViewById(R.id.textView_sparepart_price);
+
 
         }
     }
@@ -45,9 +45,10 @@ public class Spare_Parts_Adapter extends RecyclerView.Adapter<Spare_Parts_Adapte
         ExampleSpareParts currentItem = mExampleList.get(position);
 
         holder.mImageView.setImageResource(currentItem.getImageResource());
-        holder.mTextView1.setText(currentItem.getText1());
-        holder.mTextView2.setText(currentItem.getText2());
 
+
+        holder.mTextView1.setText(currentItem.getText1());
+//        holder.mTextView2.setText(currentItem.getText2());
 
     }
 
