e5668cadc7cb7e73494c771d97478e0cc92f6142
pranavkulkarni <pranav.kulkarni54@gmail.com>
2019-10-16 22:47:02 +0530

    settings activity updated

--- app/src/main/java/com/example/vehicleassistance/SettingsActivity.java
+++ app/src/main/java/com/example/vehicleassistance/SettingsActivity.java
@@ -1,59 +1,78 @@
 package com.example.vehicleassistance;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.app.AppCompatDelegate;
+import androidx.fragment.app.Fragment;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.CompoundButton;
 import android.widget.Switch;
 
-public class SettingsActivity extends AppCompatActivity {
+public class SettingsActivity extends Fragment {
 
     private Switch s1,s2;
+    private View view;
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//
+//        if(AppCompatDelegate.getDefaultNightMode()==AppCompatDelegate.MODE_NIGHT_YES){
+//            setTheme(R.style.DarkTheme);
+//        }
+//        else setTheme(R.style.SignUpTheme);
+//
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_settings);
+//
+//
+//        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+//
+//
+//        s1 =  findViewById(R.id.switch1);
+//        if(AppCompatDelegate.getDefaultNightMode()==AppCompatDelegate.MODE_NIGHT_YES){
+//            s1.setChecked(true);
+//
+//        }
+//
+//        s1.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+//            @Override
+//            public void onCheckedChanged(CompoundButton compoundButton, boolean isChecked) {
+//                if(isChecked){
+//                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+//                    restartApp();
+//                } else{
+//                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+//                    restartApp();
+//                }
+//            }
+//        });
+//
+
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-
-        if(AppCompatDelegate.getDefaultNightMode()==AppCompatDelegate.MODE_NIGHT_YES){
-            setTheme(R.style.DarkTheme);
-        }
-        else setTheme(R.style.SignUpTheme);
-
+    public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_settings);
-
-
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-
-
-        s1 =  findViewById(R.id.switch1);
-        if(AppCompatDelegate.getDefaultNightMode()==AppCompatDelegate.MODE_NIGHT_YES){
-            s1.setChecked(true);
-
-        }
-
-        s1.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-            @Override
-            public void onCheckedChanged(CompoundButton compoundButton, boolean isChecked) {
-                if(isChecked){
-                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
-                    restartApp();
-                } else{
-                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
-                    restartApp();
-                }
-            }
-        });
-
     }
 
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        view=inflater.inflate(R.layout.activity_settings,container,false);
+
+
+        return view;
+    }
 
     public void restartApp(){
-        Intent intent = new Intent(getApplicationContext(),SettingsActivity.class);
-        startActivity(intent);
-        finish();
+       // Intent intent = new Intent(getApplicationContext(),SettingsActivity.class);
+        //startActivity(intent);
+        //finish();
     }
 
 
@@ -61,7 +80,7 @@ public class SettingsActivity extends AppCompatActivity {
         int id = menu.getItemId();
 
         if (id == android.R.id.home) {
-            finish();
+          //  finish();
         }
         return true;
     }
